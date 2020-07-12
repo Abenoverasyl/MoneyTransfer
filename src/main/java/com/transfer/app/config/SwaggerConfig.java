f9c2cec7f137ce9091e9659330ffa11011bd6662
YAben <abenov.erasyl@gmail.com>
2020-07-12 17:44:23 +0600

    feature added swagger ApiInfo

--- src/main/java/com/transfer/app/config/SwaggerConfig.java
+++ src/main/java/com/transfer/app/config/SwaggerConfig.java
@@ -2,7 +2,10 @@ package com.transfer.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,5 +23,16 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.transfer.app"))
                 .paths(regex("/.*"))
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Request Processor transfer money service")
+                .version("1.0")
+                .contact(new Contact("Beeline ESB team",
+                        "https://confluence.beeline.kz/display/ESB/ESB+Main+Page+CIS",
+                        "yaben@beeline.kz"))
+                .build();
+
     }
 }
